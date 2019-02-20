4faaea0a7b5ae18d110cdfee186983ce2dc51b92
Jorge Arturo Bradic <abradic95@email.arizona.edu>
2019-02-19 23:29:38 -0700

    Content

--- dist/assets/javascript/app.js
+++ dist/assets/javascript/app.js
@@ -1,11 +1,13 @@
 var i = 0;
 var txt = "Hello. Welcome to my Portfolio";
-var speed = 50;
+var speed = 100;
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("greeting-text").innerHTML += txt.charAt(i);
+    document.getElementById("introduction-text").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }
+
+console.log("another test");
